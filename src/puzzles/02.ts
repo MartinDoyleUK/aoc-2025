a1dfaec4fdc5f78d5cb22f484043f4869042af09
Martin Doyle <github@martindoyle.com>
2025-12-02 07:30:33 +0000

    Complete day 2, setup for day 3

--- src/puzzles/02.ts
+++ src/puzzles/02.ts
@@ -1,7 +1,7 @@
 import { getDataForPuzzle, logAnswer } from '../utils/index.js';
 
 // Toggle this to use test or real data
-const USE_TEST_DATA = true;
+const USE_TEST_DATA = false;
 
 // Load data from files
 const data = getDataForPuzzle(import.meta.url);
@@ -10,14 +10,28 @@ const data = getDataForPuzzle(import.meta.url);
 const runOne = () => {
   const taskStartedAt = performance.now();
   const dataToUse = USE_TEST_DATA ? data.TEST1 : data.REAL;
-  const lines = dataToUse
+  const firstLine = dataToUse
     .split('\n')
     .map((line) => line.trim())
-    .filter((line) => line.length > 0);
+    .find((line) => line.length > 0);
+
+  const ranges = firstLine!.split(',').map((range) => range.split('-').map(Number) as [number, number]);
+  const invalidIds: number[] = [];
+  for (const range of ranges) {
+    for (let idNum = range[0]; idNum <= range[1]; idNum++) {
+      const idStr = String(idNum);
+      const len = idStr.length;
+      if (len % 2 === 0 && idStr.slice(0, len / 2) === idStr.slice(len / 2)) {
+        invalidIds.push(idNum);
+      }
+    }
+  }
+
+  const sumInvalidIds = invalidIds.reduce((prev, next) => prev + next, 0);
 
   logAnswer({
-    answer: lines.length,
-    expected: USE_TEST_DATA ? undefined : undefined,
+    answer: sumInvalidIds,
+    expected: USE_TEST_DATA ? 1_227_775_554 : 38_310_256_125,
     partNum: 1,
     taskStartedAt,
   });
@@ -27,14 +41,48 @@ const runOne = () => {
 const runTwo = () => {
   const taskStartedAt = performance.now();
   const dataToUse = USE_TEST_DATA ? data.TEST2 : data.REAL;
-  const lines = dataToUse
+  const firstLine = dataToUse
     .split('\n')
     .map((line) => line.trim())
-    .filter((line) => line.length > 0);
+    .find((line) => line.length > 0);
+
+  const isInvalid = (idNum: number): boolean => {
+    const idStr = String(idNum);
+    const len = idStr.length;
+
+    let isRepeated = false;
+    for (let numRepeats = 2; numRepeats <= len; numRepeats++) {
+      if (len % numRepeats === 0) {
+        const repeatStr = idStr.slice(0, len / numRepeats);
+        const candidate = Array.from({ length: numRepeats }).fill(repeatStr).join('');
+        // console.log({ candidate, idNum, numRepeats, repeatStr });
+        if (candidate === idStr) {
+          isRepeated = true;
+          break;
+        }
+      }
+    }
+
+    // console.log(`${idNum} ${isRepeated ? 'IS' : 'is NOT'} repeated`);
+
+    return isRepeated;
+  };
+
+  const ranges = firstLine!.split(',').map((range) => range.split('-').map(Number) as [number, number]);
+  const invalidIds: number[] = [];
+  for (const range of ranges) {
+    for (let idNum = range[0]; idNum <= range[1]; idNum++) {
+      if (isInvalid(idNum)) {
+        invalidIds.push(idNum);
+      }
+    }
+  }
+
+  const sumInvalidIds = invalidIds.reduce((prev, next) => prev + next, 0);
 
   logAnswer({
-    answer: lines.length,
-    expected: USE_TEST_DATA ? undefined : undefined,
+    answer: sumInvalidIds,
+    expected: USE_TEST_DATA ? 4_174_379_265 : undefined,
     partNum: 2,
     taskStartedAt,
   });
